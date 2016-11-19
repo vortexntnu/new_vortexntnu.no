4e779bb4dd5ad010e358a8cd0133caba38551b31
Erik Dymbe <erik.dymbe@gmail.com>
2016-11-19 17:36:31 +0100

    fikset at bakgrunnen ikke dekker siden om man zoomer ut, lagt nav-bar in i en js + lagt til en uferdig team-side

---
+++ js/template.js
@@ -0,0 +1,14 @@
+nav = document.querySelector("nav");
+//footer = document.getElementById("footer");
+
+nav.innerHTML = "\
+<a href='index.html'><img src='img/logo_og_navn.png'></a>\
+<ul>\
+    <li><a href='index.html'>HOME</a></li>\
+    <li><a href='projects.html'>PROJECTS</a></li>\
+    <li><a href='about.html'>ABOUT</a></li>\
+    <li><a href='team.html'>TEAM</a></li>\
+    <li><a href='join.html'>JOIN US</a></li>\
+</ul>"
+
+console.log(nav);
