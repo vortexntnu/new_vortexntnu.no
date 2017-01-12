3dac00df3450e052e5ad7d58494095f74c46548f
Erik Dymbe <erik.dymbe@gmail.com>
2017-01-12 02:40:33 +0100

    litt av hvert

--- js/template.js
+++ js/template.js
@@ -2,13 +2,13 @@ nav = document.querySelector("nav");
 //footer = document.getElementById("footer");
 
 nav.innerHTML = "\
-<a href='index.html'><img src='img/logo_og_navn.png'></a>\
-<ul>\
-    <li><a href='index.html'>HOME</a></li>\
-    <li><a href='projects.html'>PROJECTS</a></li>\
-    <li><a href='about.html'>ABOUT</a></li>\
-    <li><a href='team.html'>TEAM</a></li>\
-    <li><a href='join.html'>JOIN US</a></li>\
-</ul>"
+    <a href='index.html'><img src='img/logo_og_navn.png'></a>\
+    <div>\
+        <a href='index.html'>HOME</a>\
+        <a href='projects.html'>PROJECTS</a>\
+        <a href='about.html'>ABOUT</a>\
+        <a href='team.html'>TEAM</a>\
+        <a href='join.html'>JOIN US</a>\
+</div>";
 
 console.log(nav);
