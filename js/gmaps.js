b98ee1184fe4f0c1f28cd3337eeae42a8a769b6d
Erik Dymbe <erik.dymbe@gmail.com>
2017-01-21 14:41:49 +0100

    gmaps

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,7 +1,7 @@
 function initMap() {
         var kontor = {lat: 63.418551, lng: 10.399409};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 17,
+          zoom: 15,
           center: kontor
         });
         var marker = new google.maps.Marker({
