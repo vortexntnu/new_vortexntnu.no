db27a63171011a5fb4be44aaa8e4464ab259cbf5
Erik Dymbe <erik.dymbe@gmail.com>
2017-01-21 13:56:34 +0100

    stuffs

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,29 +1,39 @@
 var nav = document.querySelector("nav");
 var links = document.getElementById("link-wrapper");
-var last_known_scroll_position = 0;
+var scroll_position = window.scrollY;
 var ticking = false;
 
 nav.style.position = "fixed";
 
-function doSomething(scroll_pos) {
+if(scroll_position > 0) {
+    nav.style.backgroundColor = "#151515";
+    nav.style.height = "50px";
+    links.style.top = "7px"
+}
+
+function sticky_nav(scroll_pos) {
     if (scroll_pos == 0) {
         nav.style.backgroundColor = "transparent";
         nav.style.height = "100px";
         links.style.top = "20px";
     } else {
-        nav.style.backgroundColor = "#252525";
+        nav.style.backgroundColor = "#151515";
         nav.style.height = "50px";
         links.style.top = "7px"
     }
 }
 
-window.addEventListener('scroll', function() {
-    last_known_scroll_position = window.scrollY;
+function throttle1() {
+    scroll_position = window.scrollY;
     if (!ticking) {
-        window.requestAnimationFrame(function() {
-            doSomething(last_known_scroll_position);
-            ticking = false;
-        });
+        window.requestAnimationFrame(throttle2);
     }
     ticking = true;
-});
+}
+
+function throttle2() {
+    sticky_nav(scroll_position);
+    ticking = false;
+}
+
+window.addEventListener('scroll', throttle1);
