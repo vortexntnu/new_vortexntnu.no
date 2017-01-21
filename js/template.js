67eae6b9e613d4ab35bc600cae90524d640ced56
Erik Dymbe <erik.dymbe@gmail.com>
2017-01-21 14:13:25 +0100

    stuff

--- js/template.js
+++ js/template.js
@@ -6,8 +6,8 @@ nav.innerHTML = "\
         <a href='index.html'><img src='img/logo_og_navn.png'></a>\
         <div id='link-wrapper'>\
             <a href='index.html'>HOME</a>\
-            <a href='projects.html'>PROJECTS</a>\
             <a href='about.html'>ABOUT</a>\
+            <a href='projects.html'>PROJECTS</a>\
             <a href='team.html'>TEAM</a>\
             <a href='join.html'>JOIN US</a>\
             <a href='contact.html'>CONTACT</a>\
