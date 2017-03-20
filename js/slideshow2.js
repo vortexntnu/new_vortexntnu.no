f946055d984b2938c1c3f50b68a4d3d469a943a2
Erik Dymbe <erik.dymbe@gmail.com>
2017-03-20 03:18:45 +0100

    Slideshow på index og projects. Forslag til join siden

--- js/slideshow2.js
+++ js/slideshow2.js
@@ -1,26 +1,31 @@
-var slideIndex = 1;
-showSlides2(slideIndex);
+var slideIndex2 = 1;
+var timer2;
+
+showSlides2(slideIndex2);
 
 function plusSlides2(n) {
-    showSlides2(slideIndex += n);
+    showSlides2(slideIndex2 += n);
 }
 
 function currentSlide2(n) {
-    showSlides2(slideIndex = n);
+    showSlides2(slideIndex2 = n);
 }
 
 function showSlides2(n) {
+    clearTimeout(timer2);
+    timer2 = window.setTimeout(function() { plusSlides2(1); },5000);
+    
     var i;
     var slides = document.getElementsByClassName("mySlides2");
     var dots = document.getElementsByClassName("dot2");
     if (n > slides.length) {
-        slideIndex = 1
+        slideIndex2 = 1
     }
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex2 = slides.length
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex2 - 1].style.display = "block";
 }
