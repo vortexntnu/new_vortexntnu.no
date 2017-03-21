b3f349303f91634e9c5e5782ce627f67baf5db4e
Erik Dymbe <erik.dymbe@gmail.com>
2017-03-22 00:55:08 +0100

    imidlertidig løsning på .htaccess (index går til index.html)

--- js/template.js
+++ js/template.js
@@ -5,7 +5,7 @@ nav.innerHTML = "\
     <div id='core-nav'>\
         <a id='img-link' href='index.html'><img src='img/logo_og_navn.png'></a>\
         <div id='link-wrapper'>\
-            <a href=''>HOME</a>\
+            <a href='index'>HOME</a>\
             <a href='about'>ABOUT</a>\
             <a href='projects'>PROJECTS</a>\
             <a href='team'>TEAM</a>\
